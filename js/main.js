9f2b4c7ba5a5b6ee9d232214f2f37dda1172fb19
Freddy Guerra <arcobaleno262@gmail.com>
2022-08-06 19:17:51 -0300

    ejercicio-12 finalizado sin bonus

--- js/main.js
+++ js/main.js
@@ -32,89 +32,67 @@ console.log(
 );
 
 /* ------------------------------------------ */
+function compararApellidos(persona1, persona2) {
+  //funcion que compara Apellidos
+  let encuentra = true;
+  let posicion1 = 0; //variable que me permite almacenar las posiciones
+  let posicion2 = 2;
+  for (let i = 2; i < 4; i++) {
+    // el contador for solo pasara por las posisiones de apellido
+    encuentra = false;
+    for (let j = 2; j < 4; j++) {
+      if (persona1[i].innerText == persona2[j].innerText) {
+        encuentra = true;
+        posicion1 = i;
+        posicion2 = j;
+      }
+    }
+    if (encuentra) {
+      let colorCoincidencia = prompt(
+        "hubo coincidencias. Elija un color para destacar los nombres"
+      );
+      console.log("hubo coincidencias");
+      persona1[posicion1].style.color = colorCoincidencia;
+      persona2[posicion2].style.color = colorCoincidencia;
+    } else {
+      console.log("no encuentro coincidencia en Apellidos");
+    }
+  }
+}
 
 function compararNombres(persona1, persona2) {
+  // funcion que comparar nombres
   let encuentra = true;
+  let posicion1 = 0;
+  let posicion2 = 2;
   for (let i = 0; i < 2; i++) {
     encuentra = false;
     for (let j = 0; j < 2; j++) {
       if (persona1[i].innerText == persona2[j].innerText) {
         encuentra = true;
-        console.log(persona1[i].innerText == persona2[j].innerText);
+        posicion1 = i;
+        posicion2 = j;
       }
     }
     if (!encuentra) {
-      console.log("los arreglos no son iguales");
-      confirm("deseas comparar los apellidos?");
+      console.log("los nombres no son iguales");
+      let confirmacion = confirm("deseas comparar los apellidos?");
+      if (confirm) {
+        compararApellidos(persona1, persona2); //llama a la funcion de comparar apellidos
+      }
+      return null;
+    }
+
+    if (encuentra) {
+      let colorCoincidencia = prompt(
+        "hubo coincidencias. Elija un color para destacar los nombres"
+      );
+      console.log("hubo coincidencias");
+      persona1[posicion1].style.color = colorCoincidencia;
+      persona2[posicion2].style.color = colorCoincidencia;
       return null;
     }
   }
-  if (encuentra) {
-    let colorCoincidencia = prompt(
-      "hubo coincidencias. Elija un color para destacar los nombres"
-    );
-    console.log("hubo coincidencias");
-    return colorCoincidencia;
-  }
 }
 
-function compararNombres(listaAlum1, listaAlum2)
-
-/* let comparar1 = compararNombres(alumno1); */
-
-/* console.log(comparar1); */
-
-/*if (i1n === i2n) {
-  prompt("si son iguales. Elija un color para destacar los nombres");
-  console.log("hubo coincidencias");
-} else if (i1n === i2sn) {
-  prompt("si son iguales. Elija un color para destacar los nombres");
-  console.log("hubo coincidencias");
-  /* }else if(i1sn === i2n){
-        prompt('si son iguales. Elija un color para destacar los nombres')
-        console.log('hubo coincidencias')
-    }else if(i1sn === i2sn){
-        prompt('si son iguales. Elija un color para destacar los nombres')
-        console.log('hubo coincidencias') 
-} else {
-  console.log("No hubo coincidencias en los nombres de los integrantes");
-}
-
-window.confirm("¿Quieres comparar los apellidos?");
-
-if (i1a === i2a) {
-  prompt("si son iguales. Elija un color para destacar los nombres");
-  console.log("hubo coincidencias");
-} else if (i1a === i2sa) {
-  prompt("si son iguales. Elija un color para destacar los nombres");
-  console.log("hubo coincidencias");
-   }else if(i1sa === i2a){
-        prompt('si son iguales. Elija un color para destacar los nombres')
-        console.log('hubo coincidencias')
-    }else if(i1sa === i2sa){
-        prompt('si son iguales. Elija un color para destacar los nombres')
-        console.log('hubo coincidencias') 
-} else {
-  console.log("No hubo coincidencias en los apellidos de los integrantes");
-}
-
---------------------------------------------------------- */
-
-/* let integrantes = { 
-    integrante1: [i1n, i1sn, i1a, i1sa],
-    integrante2: [i2n, i2sn, i2a, i2sa]
-}
-
-console.log({integrantes})
-
-
-let comparacion2 = integrantes.forEach( function (igualdad) {
-    
-    if (integrante1 === integrante2){
-        console.log("hay igualdad en el nombre y/o apellido")
-
-    }else {
-        console.log("no hay igualdad en ningun paramatro")
-    }
-
-}) */
+compararNombres(listaAlum1, listaAlum2);
